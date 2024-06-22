f5e4d07692b76fbed64de1c63b465c2827724579
sowmik-sec <sowmik.sec@gmail.com>
2024-06-22 19:29:09 +0600

    Dot Vs Bracket Notation

---
+++ notation.js
@@ -0,0 +1,17 @@
+const person = {
+  name: "Ahsan Habib",
+  profession: "Unemployed",
+  age: 28,
+  28: "be employed",
+  address: "Bhurungamari",
+};
+
+// dot notation
+const prof1 = person.profession;
+
+// bracket notation
+const prof2 = person["profession"];
+console.log(prof2);
+
+const prof3 = person["28"];
+console.log(prof3);
