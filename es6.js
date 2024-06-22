88e0a8473ad2939a77345d49cfa507960bf6d886
sowmik-sec <sowmik.sec@gmail.com>
2024-06-22 16:28:45 +0600

    template string

---
+++ es6.js
@@ -0,0 +1,8 @@
+const numbers = [89, 35, 88, 12];
+const student = {
+  name: "Sakib Khan",
+  age: 45,
+};
+
+const about = `My Name is ${student.name} age is ${student.age} has number ${numbers[2]}`;
+console.log(about);
